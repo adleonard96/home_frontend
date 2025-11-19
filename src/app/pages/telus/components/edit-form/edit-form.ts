9d2b55645613484f50973a029cab87e8c7e25205
adleonard96 <alexanderleonard27@gmail.com>
2025-11-19 18:39:43 -0500

    values show up in the box on edit

--- src/app/pages/telus/components/edit-form/edit-form.ts
+++ src/app/pages/telus/components/edit-form/edit-form.ts
@@ -1,34 +1,45 @@
-import { Component, inject, Inject, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, inject } from '@angular/core';
 import { TelusService } from '../../services/Telus.service';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-form',
+  standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './edit-form.html',
   styleUrl: './edit-form.css',
 })
-export class EditForm {
+export class EditForm implements OnChanges {
   @Input() id: number = Infinity;
-  @Input() start: string  | undefined= '';
-  @Input() stop: string | null | undefined= null;
-  @Input() dayOfWeek: string | undefined= '';
+  @Input() start: string | undefined = '';
+  @Input() stop: string | null | undefined = null;
+  @Input() dayOfWeek: string | undefined = '';
   @Input() closeFn: any;
   @Input() updateEvent: any;
 
-  formStart = new FormControl(new Date());
-  formStop = new FormControl(new Date());
-
+  formStart = new FormControl<string | null>(null);
+  formStop = new FormControl<string | null>(null);
 
   private service = inject(TelusService);
 
-  close() {
-    if (this.closeFn) {
-      this.closeFn();
-    }
+  ngOnChanges() {
+    this.formStart.setValue(this.toLocalInputValue(this.start));
+    this.formStop.setValue(this.toLocalInputValue(this.stop));
+  }
+  private toLocalInputValue(dateString?: string | null): string {
+    if (!dateString) return '';
+    const date = new Date(dateString);
+    const local = new Date(date.getTime() - date.getTimezoneOffset() * 60000);
+    return local.toISOString().slice(0, 16); // yyyy-MM-ddTHH:mm
   }
 
-  submit(start: string, stop: string) {
-    let updatedEvent = this.service.updateWorkEvent(this.id, new Date(start), new Date(stop));
+  close() {
+    this.closeFn?.();
   }
+
+  // submit() {
+  //   if (!this.formStart.value || !this.formStop.value) return;
+
+  //   this.service.updateWorkEvent(this.id, this.formStart.value, this.formStop.value);
+  // }
 }
