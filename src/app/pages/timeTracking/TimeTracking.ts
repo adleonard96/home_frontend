ffcb2596e9f5258422f715d5ec2dcb78961fdea0
adleonard96 <alexanderleonard27@gmail.com>
2025-11-29 20:37:16 -0500

    moved functionality out of constructor to make more reusable

--- src/app/pages/timeTracking/TimeTracking.ts
+++ src/app/pages/timeTracking/TimeTracking.ts
@@ -13,7 +13,6 @@ import { HttpClient } from '@angular/common/http';
 import { TimeTrackingService } from './services/TimeTracking.service';
 import { concatMap, map, Observable, of, scan } from 'rxjs';
 import { HttpResponses } from './models/HttpResponses';
-import { error } from 'node:console';
 import { WorkEvent } from './components/work-event/work-event';
 import { AsyncPipe } from '@angular/common';
 
@@ -32,18 +31,37 @@ export class TimeTracking {
   events: WritableSignal<HttpResponses.event[]> = signal([]);
   todaysEvents: WritableSignal<HttpResponses.event[]> = signal([]);
   hoursToday: WritableSignal<number> = signal(0);
+  today = new Date();
   constructor() {
     effect(() => {
       let todayEpoch = Date.now();
-      let today = new Date();
-      let daysFromSunday = today.getDay();
+      let daysFromSunday = this.today.getDay();
       const SATURDAY_NO = 6;
-      let daysToSaturday = SATURDAY_NO - today.getDay();
+      let daysToSaturday = SATURDAY_NO - this.today.getDay();
 
       let DAY_IN_MS = 86_400_000;
       let sunday = new Date(todayEpoch - daysFromSunday * DAY_IN_MS);
       let saturday = new Date(todayEpoch + daysToSaturday * DAY_IN_MS);
 
+
+    });
+  }
+
+  creatWorkEvent() {
+    let res = this.service.createWorkEvent(new Date());
+
+    res.subscribe((value) => {
+      if (value) {
+        this.events.set([value, ...this.events()]);
+      }
+    });
+  }
+
+  deleteWorkEvent(id: number) {
+    this.events.set(this.events().filter((event) => event.id != id));
+  }
+
+  setWorkEvents(saturday: Date, sunday: Date) {
       this.service.getWorkEvents(sunday, saturday).subscribe(
         (value) => {
           if (value.length > 0) {
@@ -54,9 +72,9 @@ export class TimeTracking {
               this.events().filter((event) => {
                 let eventDate = new Date(event.start);
                 return (
-                  eventDate.getDate() == today.getDate() &&
-                  eventDate.getMonth() == today.getMonth() &&
-                  eventDate.getFullYear() == today.getFullYear() &&
+                  eventDate.getDate() == this.today.getDate() &&
+                  eventDate.getMonth() == this.today.getMonth() &&
+                  eventDate.getFullYear() == this.today.getFullYear() &&
                   event.stop !== undefined
                 );
               })
@@ -73,21 +91,6 @@ export class TimeTracking {
         },
         (error) => console.error(error)
       );
-    });
-  }
-
-  creatWorkEvent() {
-    let res = this.service.createWorkEvent(new Date());
-
-    res.subscribe((value) => {
-      if (value) {
-        this.events.set([value, ...this.events()]);
-      }
-    });
-  }
-
-  deleteWorkEvent(id: number) {
-    this.events.set(this.events().filter((event) => event.id != id));
   }
 
   brCheck = "";
